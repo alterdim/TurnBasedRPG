ae11d972ca9c569413abcb334ad06123819a6af8
Alternis <roilouisdim@gmail.com>
2020-08-10 23:20:48 +0200

    k

--- source/entities/Player.hx
+++ source/entities/Player.hx
@@ -1,15 +1,21 @@
 package entities;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.math.FlxPoint;
 
 class Player extends Entity
 {
-	static inline var SPEED:Float = 200;
+	static inline var SPEED:Float = 120;
 
 	public function new(x:Float = 0, y:Float = 0)
 	{
-		super(x, y, "player");
+		super(x, y, "soldierwalking");
+		setFacingFlip(FlxObject.LEFT, false, false);
+		setFacingFlip(FlxObject.RIGHT, true, false);
+		animation.add("lr", [3, 4], 6, false);
+		animation.add("u", [5, 6, 5, 7], 6, false);
+		animation.add("d", [0, 1, 0, 2], 6, false);
 		this.hp = 5;
 		atk = agi = int = def = 2;
 		mp = 3;
@@ -31,10 +37,20 @@ class Player extends Entity
 		var left:Bool = false;
 		var right:Bool = false;
 
+		#if FLX_KEYBOARD
 		up = FlxG.keys.anyPressed([UP, W]);
 		down = FlxG.keys.anyPressed([DOWN, S]);
 		left = FlxG.keys.anyPressed([LEFT, A]);
 		right = FlxG.keys.anyPressed([RIGHT, D]);
+		#end
+
+		#if mobile
+		var virtualPad = PlayState.virtualPad;
+		up = up || virtualPad.buttonUp.pressed;
+		down = down || virtualPad.buttonDown.pressed;
+		left = left || virtualPad.buttonLeft.pressed;
+		right = right || virtualPad.buttonRight.pressed;
+		#end
 
 		if (up && down)
 			up = down = false;
@@ -43,8 +59,6 @@ class Player extends Entity
 
 		if (up || down || left || right)
 		{
-			velocity.x = SPEED;
-			velocity.y = SPEED;
 			var newAngle:Float = 0;
 			if (up)
 			{
@@ -53,6 +67,7 @@ class Player extends Entity
 					newAngle -= 45;
 				else if (right)
 					newAngle += 45;
+				facing = FlxObject.UP;
 			}
 			else if (down)
 			{
@@ -61,13 +76,36 @@ class Player extends Entity
 					newAngle += 45;
 				else if (right)
 					newAngle -= 45;
+				facing = FlxObject.DOWN;
 			}
 			else if (left)
+			{
 				newAngle = 180;
+				facing = FlxObject.LEFT;
+			}
 			else if (right)
+			{
 				newAngle = 0;
+				facing = FlxObject.RIGHT;
+			}
+
+			// determine our velocity based on angle and speed
 			velocity.set(SPEED, 0);
 			velocity.rotate(FlxPoint.weak(0, 0), newAngle);
+
+			// if the player is moving (velocity is not 0 for either axis), we need to change the animation to match their facing
+			if ((velocity.x != 0 || velocity.y != 0) && touching == FlxObject.NONE)
+			{
+				switch (facing)
+				{
+					case FlxObject.LEFT, FlxObject.RIGHT:
+						animation.play("lr");
+					case FlxObject.UP:
+						animation.play("u");
+					case FlxObject.DOWN:
+						animation.play("d");
+				}
+			}
 		}
 	}
 }
